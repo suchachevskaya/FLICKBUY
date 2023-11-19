6bb54e849836f19772fb95ebe71a2052d22dd276
suchachevskaya <116747974+suchachevskaya@users.noreply.github.com>
2023-11-19 12:03:44 -0800

    32п

--- resources/views/ticket/create.blade.php
+++ resources/views/ticket/create.blade.php
@@ -13,7 +13,7 @@
                     <div>
                         <div class="mt-4">
                             <x-input-label for="description" :value="__('Description')"/>
-                            <x-textarea name="description" id="description"/>
+                            <x-textarea name="description" id="description" value=""/>
                             <x-input-error :messages="$errors->get('description')" class="mt*2"/>
                         </div>
                         <div class="mt-4">
